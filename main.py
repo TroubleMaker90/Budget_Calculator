3aa78428bfd288ca287924e34039943e0cf86713
TroubleMaker90 <d.nov48@mail.ru>
2025-08-04 18:54:36 +0300

    Добавлена функция расхода (пока только текст)

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 
-#Очистка консоли для удобства использования
+#Очистка консоли для удобства использования !! пока не используется
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+#Главное меню калькулятора бюджета
 def main_menu():
     while True:
         #Стартовое оповещение пользователя при запуске
@@ -36,6 +37,7 @@ def main_menu():
         else:
             print('Некорректная команда! Попробуйте снова.')
 
+#Добавление дохода
 def add_income():
     print()
     while True:
@@ -63,6 +65,42 @@ def add_income():
             print('Ошибка! Сумма дохода должна быть числом!')
     question_for_user()
 
+#Добавление расхода
+def add_expense():
+    print()
+    while True:
+        expense_type = input('Введите тип расхода (например: ЖКХ, налог, покупка): ')
+        if expense_type.isalpha():
+            break
+        else:
+            print('Ошибка! Введите тип расхода.')
+            print()
+
+    while True:
+        try:
+            print()
+            expense_amount = float(input('Введите сумму расхода: '))
+
+            if expense_amount >= 0:
+                print('--------------')
+                print('Расход успешно записан!\nПосмотреть Расходы можно в статистике')
+                print('--------------')
+                break
+            else:
+                print('Сумма Расхода должна быть больше либо равна нулю!')
+
+        except ValueError:
+            print('Ошибка! Сумма расхода должна быть числом!')
+    question_for_user()
+
+#Отображение статистики
+def show_statistics():
+    pass
+
+
+#НЕ ОСНОВНЫЕ ФУНКЦИИ
+
+#Вопрос пользователю о продолжении использования
 def question_for_user():
     while True:
         print()
@@ -78,16 +116,12 @@ def question_for_user():
             print('')
             print('ОШИБКА! Введите "Да" или "Нет" чтобы продолжить')
 
-def add_expense():
-    pass
-
-def show_statistics():
-    pass
-
+#Выход из программы
 def calculator_exit():
     print('========================================================')
     print('                  До новых встреч!')
     print('========================================================')
     exit()
 
+#Запуск стартового меню
 main_menu()
